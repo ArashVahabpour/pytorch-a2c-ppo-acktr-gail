e541c63dceb7567abc07d45baadffabf9f578925
arash <vahaabpour@gmail.com>
2020-12-08 09:10:43 -0800

    sog module

--- a2c_ppo_acktr/utils.py
+++ a2c_ppo_acktr/utils.py
@@ -63,3 +63,49 @@ def cleanup_log_dir(log_dir):
         files = glob.glob(os.path.join(log_dir, '*.monitor.csv'))
         for f in files:
             os.remove(f)
+
+
+def generate_latent_codes(args, count):
+    n = args.latent_size
+    return torch.eye(n, device=args.device)[torch.randint(n, (count,))]
+
+
+criterion = nn.MSELoss(reduction='none')
+
+
+def resolve_latent_code(actor_critic, state, action, latent_size):
+    batch_size = len(state)
+    latent_batch_size = latent_size
+
+    device = state.device
+
+    # batch_size x latent_batch_size x variable_dim
+    all_z = torch.eye(latent_size, device=device).unsqueeze(0).expand(batch_size, -1, -1)
+    all_state = state.unsqueeze(1).expand(-1, latent_batch_size, -1)
+    all_action = action.unsqueeze(1).expand(-1, latent_batch_size, -1)
+
+    with torch.no_grad():
+        policy_action_all = actor_critic.act(all_state.reshape(batch_size * latent_batch_size, -1),
+                                             all_z.reshape(batch_size * latent_batch_size, -1),
+                                             None, deterministic=True)[1].reshape(batch_size, latent_batch_size, -1)
+
+    # batch_size x latent_batch_size x action_dim
+    loss = criterion(policy_action_all, all_action)
+
+    # batch_size x latent_batch_size
+    loss = loss.mean(dim=2)
+
+    # batch_size
+    _, argmin = loss.min(dim=1)
+
+    # new_z: batch_size x latent_batch_size x n_latent
+    # best_idx: batch_size x 1 x n_latent
+    best_idx = argmin[:, None, None].repeat(1, 1, latent_size)
+
+    # batch_size x 1 x n_latent
+    best_z = torch.gather(all_z, 1, best_idx)
+
+    # batch_size x n_latent
+    best_z = best_z.squeeze(1)
+
+    return best_z
